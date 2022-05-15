c270d9b9bdfa293a19f8fca305de5b376558c265
wkdtndgns <wkdtndgns@naver.com>
2022-05-16 02:47:01 +0900

    test

--- src/main/java/com/example/test2/Test2Application.java
+++ src/main/java/com/example/test2/Test2Application.java
@@ -2,9 +2,17 @@ package com.example.test2;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class Test2Application {
+
+    @RequestMapping("/")
+    String home() {
+        return "Hello World!";
+    }
 
     public static void main(String[] args) {
         System.out.println("test");
