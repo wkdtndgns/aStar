59079d62b51295e78cb1e0bd916e432b3c1d7074
wkdtndgns <wkdtndgns@naver.com>
2022-05-16 01:31:41 +0900

    Initial commit

---
+++ src/main/java/com/example/test2/Test2Application.java
@@ -0,0 +1,14 @@
+package com.example.test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Test2Application {
+
+    public static void main(String[] args) {
+        System.out.println("test");
+        SpringApplication.run(Test2Application.class, args);
+    }
+
+}
