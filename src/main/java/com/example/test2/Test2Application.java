4f622402246fa558f4d5146413901bce115fb63f
wkdtndgns <wkdtndgns@naver.com>
2022-05-16 13:41:37 +0900

    test 파일 변경

--- src/main/java/com/example/test2/Test2Application.java
+++ src/main/java/com/example/test2/Test2Application.java
@@ -8,14 +8,8 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @SpringBootApplication
 public class Test2Application {
-
-    @RequestMapping("/")
-    String home() {
-        return "Hello World!";
-    }
-
     public static void main(String[] args) {
-        System.out.println("test112");
+        System.out.println("test1123");
         SpringApplication.run(Test2Application.class, args);
     }
 
--- src/main/java/com/example/test2/controller/testController.java
+++ src/main/java/com/example/test2/controller/testController.java
@@ -3,12 +3,10 @@ package com.example.test2.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 @Controller
 public class testController {
-
-    @RequestMapping("/test")
+    @GetMapping("/test")
     public String test() {
         return "test";
     }
