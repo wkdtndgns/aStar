40e9c0a73b57e77e3d34f5bed875fb3db8f9f0d5
MinsooKim <94734394+geodo2@users.noreply.github.com>
2023-05-17 09:47:54 +0900

    Add files via upload

---
+++ aStar-minsuB_test/src/main/java/com/example/test2/Test2Application.java
@@ -0,0 +1,15 @@
+package com.example.test2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@SpringBootApplication
+public class Test2Application {
+    public static void main(String[] args) {
+        System.out.println("test1123");
+        SpringApplication.run(Test2Application.class, args);
+    }
+}
